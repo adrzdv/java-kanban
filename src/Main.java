475223c08f61d15322af5de516d3c6aaa379a15b
adrzdv <anton.drozdov2015@yandex.ru>
2024-06-22 20:46:02 +0300

    испрвлены ошибки по итогам CheckStyle

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
-import taskmanager.FileBackedTaskManager;
-import taskmanager.InMemoryTaskManager;
-
-import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    private static InMemoryTaskManager taskManager = new InMemoryTaskManager();
-    private final static File fileForBackup = new File("c:\\dev\\java-kanban\\file.txt");
-    private static FileBackedTaskManager fileManager = new FileBackedTaskManager(fileForBackup);
-
 
     public static void main(String[] args) throws IOException {
 
