55c199cd0e27f5c5f2454b2af620113c1a8825c9
adrzdv <anton.drozdov2015@yandex.ru>
2024-06-22 20:43:47 +0300

    испрвлены ошибки по итогам CheckStyle

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,6 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    private static InMemoryTaskManager taskManager = new InMemoryTaskManager();
-    private final static File fileForBackup = new File("c:\\dev\\java-kanban\\file.txt");
-    private static FileBackedTaskManager fileManager = new FileBackedTaskManager(fileForBackup);
-
 
     public static void main(String[] args) throws IOException {
 
